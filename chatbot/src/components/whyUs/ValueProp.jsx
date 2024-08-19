020a06f322feaa1e907bb8c4f51c5fa575fd3a66
asino-nelson <asinonelson276@gmail.com>
2024-08-19 20:48:29 +0300

    Text size update

--- chatbot/src/components/whyUs/ValueProp.jsx
+++ chatbot/src/components/whyUs/ValueProp.jsx
@@ -4,7 +4,7 @@ const ValueProp = () => {
   return (
     <div className="bg-[#F6F3F7] w-full h-[534px] p-10">
       <div className="py-8 px-16 gap-4">
-        <h2 className="font-semibold text-2xl">WHY US</h2>
+        <h2 className="font-semibold text-3xl">WHY US</h2>
         <p>Why does your business need ChatterBot?</p>
       </div>
       <ValuePropCard />
