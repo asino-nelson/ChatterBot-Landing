06de2094550f2666f466c6ca88da5f1e48dbc021
asino-nelson <asinonelson276@gmail.com>
2024-08-20 11:21:08 +0300

    useMediaQuery hook

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,9 +1,11 @@
 import Footer from "./components/footer/Footer";
+import NavBar from "./components/navbar/NavBar";
 import Landing from "./pages/landing/Landing";
 
 export default function App() {
   return (
     <div>
+      <NavBar/>
       <Landing />
       <Footer/>
     </div>
