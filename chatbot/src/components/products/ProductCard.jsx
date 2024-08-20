954886b0ad10f8bc17264e76d435c90729f8b1e2
asino-nelson <asinonelson276@gmail.com>
2024-08-20 11:58:14 +0300

    Product card width update

--- chatbot/src/components/products/ProductCard.jsx
+++ chatbot/src/components/products/ProductCard.jsx
@@ -14,8 +14,8 @@ const ProductCard = () => {
   return (
     <div className="grid items-center grid-cols-1 lg:grid-cols-2 w-full">
       {CardData.map((data) => (
-        <div key={data.icon} className=" h-[320px] px-8 py-4">
-          <div className="px-8 py-4 gap-4 border border-[#E1E4E5] rounded-md h-72 w-[616px]">
+        <div key={data.icon} className=" h-[320px] w-full px-8 py-4">
+          <div className="px-8 py-4 gap-4 border border-[#E1E4E5] rounded-md h-72 w-full">
             <div className="gap-2 text-[#1E715D]">
               <h2 className="text-2xl">{data.title}</h2>
               <div className="bg-[#1E715D] h-1 w-full mt-7"></div>
