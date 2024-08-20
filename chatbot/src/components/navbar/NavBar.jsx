3b719419845935a7519ede1456dc1220c031d7c6
asino-nelson <asinonelson276@gmail.com>
2024-08-20 13:47:39 +0300

    Mobile nav menu update

--- chatbot/src/components/navbar/NavBar.jsx
+++ chatbot/src/components/navbar/NavBar.jsx
@@ -11,7 +11,7 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   return (
     <AnchorLink
       className={`${
-        selectedPage === lowerCasePage ? "text-violet-900" : "text-gray-500"
+        selectedPage === lowerCasePage ? "text-violet-900" : "text-gray-700"
       }
          hover:text-violet-900  transition duration-500`}
       href={`#${lowerCasePage}`}
@@ -58,7 +58,7 @@ const NavBar = ({ selectedPage, setSelectedPage }) => {
         ) : (
           <motion.button
             alt="menu"
-            className="rounded-full bg-green-500 p-3 hover:bg-green-400 transition duration-100"
+            className="rounded-full text-violet-900 p-3 hover:text-violet-500 transition duration-100"
             whileTap={{ scale: 0.9 }}
             onClick={() => setIsMenuToggled(!isMenuToggled)}
           >
@@ -68,19 +68,19 @@ const NavBar = ({ selectedPage, setSelectedPage }) => {
 
         {/* MOILE MENU POUP */}
         {!isAboveSmallScreen && isMenuToggled && (
-          <div className="fixed right-0 bottom-0 h-full bg-cyan-950 w-[300px]">
+          <div className="fixed right-0 bottom-0 h-full bg-violet-400 w-full">
             <div className="flex justify-end p-12">
               {/**Close btn */}
               <motion.button
                 alt="menu"
-                className="rounded-full bg-green-500 p-3 hover:bg-green-400 transition duration-100"
+                className="rounded-full text-violet-900 p-3 hover:text-violet-500 transition duration-100"
                 whileTap={{ scale: 0.9 }}
                 onClick={() => setIsMenuToggled(!isMenuToggled)}
               >
                 <AiOutlineClose />
               </motion.button>
             </div>
-            <div className="flex flex-col gap-10 ml-[33%] text-xl text-gray-300">
+            <div className="flex flex-col gap-10 ml-[33%] text-xl">
               <Link
                 page="ABOUT"
                 selectedPage={selectedPage}
